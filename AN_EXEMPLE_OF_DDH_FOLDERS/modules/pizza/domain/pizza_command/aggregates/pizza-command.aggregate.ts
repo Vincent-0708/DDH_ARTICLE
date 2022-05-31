be90166d29f343a20fed2bbde1db3ca4353434c2
Vincent Flattot <vincentf@sipios.com>
2022-05-31 19:05:52 +0200

    feat(Pizza command): create the aggregate

---
+++ AN_EXEMPLE_OF_DDH_FOLDERS/modules/pizza/domain/pizza_command/aggregates/pizza-command.aggregate.ts
@@ -0,0 +1,53 @@
+import { Injectable } from "@nestjs/common";
+import Result from "../../../../../libs/result/result";
+import { PizzaCommandDataAreNotValid } from "../../../errors/pizza-command.error";
+import { PizzaEntity } from "../../create_pizza/entities/pizza.entity";
+import { PizzaCommandPrice } from "../value-objects/pizza-command-price.value-object";
+
+export type PizzaCommandAggregateAttributes = {
+    clientId : string;
+    content :PizzaEntity["id"][];
+    price: PizzaCommandPrice;
+}
+
+@Injectable()
+export class PizzaCommandAggregate {
+    protected clientId : PizzaCommandAggregateAttributes["clientId"];
+    protected content: PizzaCommandAggregateAttributes["content"];
+    protected price: PizzaCommandAggregateAttributes["price"];
+
+    private constructor(pizzaCommand:PizzaCommandAggregateAttributes){
+        this.clientId = pizzaCommand.clientId;
+        this.content = pizzaCommand.content;
+    }
+
+    public static createPizzaCommand(pizzaCommand:PizzaCommandAggregateAttributes) : 
+    Result<PizzaCommandAggregate,PizzaCommandDataAreNotValid>
+    {
+        if (!PizzaCommandAggregate.validate(pizzaCommand)){
+            return Result.err(new PizzaCommandDataAreNotValid());
+        } else {
+            return Result.ok(new PizzaCommandAggregate( {...pizzaCommand} ));
+        }
+    }
+
+    protected static validate(pizzaCommand:Partial<PizzaCommandAggregateAttributes>):boolean{
+        //validation rules to respect domain invariants 
+        return(!!pizzaCommand);
+    }
+
+    protected calculatePizzaCommandPrice():void{
+        // Business logic to calculate a command price
+        const commandPrice: PizzaCommandPrice = "price";
+        this.price = commandPrice; 
+    }
+
+    public addPizzaToContent(_pizza:PizzaEntity):void{
+        // logic to add a pizza to the pizza attribute AND to update the price 
+    }
+
+    public getClientId():PizzaCommandAggregateAttributes["clientId"] {
+        return this.clientId.toString();
+    }
+}
+
---
+++ AN_EXEMPLE_OF_DDH_FOLDERS/modules/pizza/errors/pizza-command.error.ts
@@ -0,0 +1,20 @@
+import { IngredientEntity } from "../domain/create_pizza/entities/ingredient.entity";
+
+export class PizzaCommandDataAreNotValid extends Error{
+    static readonly message = "Data are not valid";
+    public readonly code = "PIZZA_COMMAND_DATA_INVALID"
+
+    constructor(){
+        super(PizzaCommandDataAreNotValid.message);
+    }
+}
+
+export class IngredientMissingForPizza extends Error{
+    static readonly message = "{ingredient} are missing";
+    public readonly code = "PIZZA_COMMAND_DATA_INVALID"
+
+    constructor(ingredient:IngredientEntity){
+        const messageWithIngredientName = PizzaCommandDataAreNotValid.message.replace("{ingredient}",ingredient.getName())
+        super(messageWithIngredientName);
+    }
+}
